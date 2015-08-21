6493f1581812184098a325e3b494f20d98b51c12
Jamie Counsell <jamiecounsell@me.com>
2015-08-20 21:20:35 -0400

    move files around and add grunt support

--- app/procurement/settings.py
+++ app/procurement/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_PATH = BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'procurement'
 )
 
 MIDDLEWARE_CLASSES = (
