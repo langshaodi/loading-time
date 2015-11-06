d2342228be96a79b0dddd0f6ff013e5cad3be0c7
Jamie Counsell <jamiecounsell@me.com>
2015-11-05 20:55:10 -0500

    trying build script

--- build_prod.sh
+++ build_prod.sh
@@ -1,3 +1,9 @@
-ssh web@104.236.21.37
-ls
-
+SCRIPT = "
+cd loading-time;
+npm install;
+bower install;
+pip install -r requirements.txt;
+./app/manage.py migrate
+echo "Done."
+"
+ssh web@104.236.21.37 "$(SCRIPT)"
