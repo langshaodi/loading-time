cb1e6c8acc3c6143a6db384ede60426f6f36f225
Jamie Counsell <jamiecounsell@me.com>
2015-11-06 13:55:41 -0500

    I think this should work

--- build_prod.sh
+++ build_prod.sh
@@ -1,10 +1,8 @@
-cd "loading-times/"
-pwd
 git pull origin master
 source ~/.bashrc
 workon loading-time
 npm install
 bower install
 pip install -r requirements.txt
+gulp build
 ./app/manage.py migrate
-echo "done!"
