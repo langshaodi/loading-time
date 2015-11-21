58edf94474e3c1eae264f527b58ef1e93b3a573d
Jamie Counsell <jamiecounsell@me.com>
2015-11-20 23:06:52 -0500

    added build for question defaults

---
+++ app/core/management/commands/build_questions.py
@@ -0,0 +1,60 @@
+from django.core.management.base import BaseCommand
+from puzzles.models import Puzzle, PuzzleAnswerOption
+import os
+
+
+def get_or_create(model, **kwargs):
+    try:
+        m = model.objects.get(**kwargs), 0
+        return m
+    except model.DoesNotExist:
+        return model.objects.create(**kwargs), 1
+
+
+class Command(BaseCommand):
+    help = 'Builds the question database'
+
+    def handle(self, *args, **options):
+        print os.path.dirname(
+            os.path.realpath(__file__))
+        f = os.path.join(os.path.dirname(
+            os.path.realpath(__file__)), '..', 'data/questions.txt')
+        f = open(f, 'r')
+        lines = f.read().split("\n")
+
+        to_obj = []
+
+        for l in lines:
+            try:
+                question, answer = l.split("ANS")
+                a, b, x, y = [r.strip().lower() if r.strip(
+                ) != "_" else None for r in
+                    question.replace(' is to ', '-').replace(
+                    ' as ', '-').split("-")]
+                answers = [(ans.strip().lower(), True) if ans.strip(
+                )[0].isupper() else (
+                    ans.strip(), False) for ans in
+                    answer.strip().split(",")]
+            except Exception as e:
+                print e
+                raise Exception("Invalid format: {}".format(l))
+            if True not in [r[1] for r in answers]:
+                raise Exception("No correct answer indicated: {}".format(l))
+            to_obj.append((a, b, x, y, answers))
+
+        for obj in to_obj:
+            a, x, b, y, answers = obj
+            puzzle, c = get_or_create(
+                Puzzle,
+                comparison_a_1=a,
+                comparison_a_2=b,
+                comparison_b_1=x,
+                comparison_b_2=y
+            )
+            for a in answers:
+                ans = get_or_create(
+                    PuzzleAnswerOption,
+                    puzzle=puzzle,
+                    text=a[0],
+                    correct=a[1]
+                )
