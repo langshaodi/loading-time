46466dcf4d77f4170dd0aff309918c41f432e2ba
Jamie Counsell <jamiecounsell@me.com>
2015-11-29 14:44:40 -0500

    rm debug setting URL

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,19 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from core.views import index, urls_as_view
-from django.conf import settings
 from games.views import GameView, DefaultGameView
 from responses.views import ResponseView
 
-
-if settings.DEBUG:
-    urlpatterns = [
-        url(r'^$', index),
-    ]
-else:
-    urlpatterns = []
+urlpatterns = []
 
 apipatterns = [
+    url(r'^$', index),
     url(r'^game/$', GameView.as_view()),
     url(r'^game/default/$', DefaultGameView.as_view()),
     url(r'^response/$', ResponseView.as_view())
