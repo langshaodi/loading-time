c9d85405ca97ba3a780a9b82d208c39567682fb1
Jamie Counsell <jamiecounsell@me.com>
2015-11-30 16:00:28 -0500

    add verbose response id

--- app/responses/models.py
+++ app/responses/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class Response(models.Model):
     submitted = models.DateTimeField(auto_now_add=True)
 
+    def __repr__(self):
+        return "Response {}".format(self.pk)
+
+    def __str__(self):
+        return self.__repr__()
+
 
 class GameResponse(models.Model):
     response = models.ForeignKey(Response)
